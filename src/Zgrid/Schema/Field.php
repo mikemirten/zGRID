37f20b965ba5c3b2f5a4777eeb03b1d6929a0c81
mikemirten <mikemirten@gmail.com>
2016-05-14 00:50:12 -0400

    Auto title handling improved

--- src/Zgrid/Schema/Field.php
+++ src/Zgrid/Schema/Field.php
@@ -174,7 +174,7 @@ class Field
 	public function getTitle()
 	{
 		if ($this->title === null) {
-			return ucfirst($this->name);
+			return ucwords(preg_replace('~([a-z]+)([A-Z]+)~', '$1 $2', $this->name));
 		}
 
 		return $this->title;
